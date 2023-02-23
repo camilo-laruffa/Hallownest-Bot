87de564592b1c335f48f276c6cbc14dac59da253
Camilo Laruffa <camilolaruffa@gmail.com>
2023-02-23 18:33:47 -0300

    Version 2
    Ahora manda un mensaje embed

--- library/bot/__init__.py
+++ library/bot/__init__.py
@@ -1,6 +1,8 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+from discord import Embed
 from discord import Intents 
+from datetime import datetime
 
 
 PREFIX = "+"
@@ -14,7 +16,11 @@ class Bot(BotBase):
         self.guild = None
         self.schedule = AsyncIOScheduler()
 
-        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS, intents=Intents.default())
+        super().__init__(
+            command_prefix=PREFIX, 
+            owner_ids=OWNER_IDS, 
+            intents=Intents.all()
+        )
 
     def run(self, version):
         self.VERSION = version
@@ -31,12 +37,31 @@ class Bot(BotBase):
     async def on_disconnected(self):
         print("bot disconnected")
 
+
     async def on_ready(self):
         if not self.ready:
             self.ready = True
             #Cambiar esta ID por la del server de hallownest cuando este listo para el deploy (Ver minuto 10:30 de https://www.youtube.com/watch?v=jVnEqd8bnBk&list=PLYeOw6sTSy6ZGyygcbta7GcpI8a5-Cooc&index=6)
             self.guild = self.get_guild(414473395082887168)
             print("bot ready")        
+
+            # Obtiene el canal con X id y manda un mensaje 
+            channel = self.get_channel(540398488333189120)
+            #await channel.send("Now online!")
+
+            #Este es un mensaje embed que son esos mensajes locos que hacen los bots
+            embed = Embed(title="Now online!", description="Hallow bot is at your service now.", colour=0x05ffe2, timestamp=datetime.now())
+            fields = [("Owner","Camilo Laruffa",True), 
+                        ("Server", self.guild.name, True),
+                        ("Caidas de Tomás", "26", False)]
+            for name,value,inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="Camilo Laruffa", icon_url="https://cdn.discordapp.com/attachments/414473395082887170/1078427753893867520/image.png")
+            embed.set_footer(text="chupen bolas")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/414473395082887170/1078428020802584576/image.png")
+            embed.set_image(url=self.guild.icon)
+            await channel.send(embed=embed)
+                
         else:
             print("bot reconnected")
 
